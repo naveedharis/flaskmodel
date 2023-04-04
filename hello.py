0f347f09dee1501a7a1086b732ebed448ae2c4f9
Haris Naveed <68024767+naveedharis@users.noreply.github.com>
2023-04-04 18:44:12 -0500

    editing hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import request
 from model import run_model
 import json
 
@@ -16,4 +16,4 @@ def hello_world():
     return results
 
 if __name__ == "__main__": 
-	app.run(port=5000) 
+	app.run(host = "0.0.0.0", port = 5000, debug = True) 
