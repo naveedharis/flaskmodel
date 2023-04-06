a0034d8878b4e68490e172b51d2225cd41b663da
Haris Naveed <68024767+naveedharis@users.noreply.github.com>
2023-04-06 13:00:48 -0500

    Changed flask python file

---
+++ recommend.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from model import run_model
+import json
+from waitress import serve
+
+app = Flask(__name__)
+
+#@app.route('/') 
+#def index(): 
+#    return "Flask server" 
+
+#@app.route("/entermovietitle", methods=['POST'])
+@app.route("/", methods=['POST'])
+def recommend_movie():
+    data = request.get_json()
+    results = run_model(data['title'])
+    #print(results)
+    return results
+
+if __name__ == "__main__": 
+	app.run()
