2284bb0169e822dbc57ae9e16ebd9ec9ccaa67fa
Haris Naveed <68024767+naveedharis@users.noreply.github.com>
2023-04-08 07:30:05 -0500

    Modified recommend.py

--- recommend.py
+++ recommend.py
@@ -3,14 +3,8 @@ from model import run_model
 
 app = Flask(__name__)
 
-#@app.route('/') 
-#def index(): 
-#    return "Flask server" 
-
-#@app.route("/entermovietitle", methods=['POST'])
 @app.route("/", methods=['POST'])
 def recommend_movie():
     data = request.get_json()
     results = run_model(data['title'])
-    #print(results)
     return results
