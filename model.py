9099f503dbac8fafd812960dc6e1c1e4781c0e0d
Haris Naveed <68024767+naveedharis@users.noreply.github.com>
2023-04-08 05:43:26 -0500

    Modify the python file

--- model.py
+++ model.py
@@ -1,24 +1,7 @@
 import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt
-import os
-import string
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
-data = pd.read_csv('tmdb_movie_info.csv')
-data = data.drop(
-        columns=['adult', 'video', 'backdrop_path', 'poster_path', 'Unnamed: 0', 'release_date', 'original_title'])
-features = ['keywords', 'genres', 'top_cast']
-
-def clean_data(x):
-    x = str.lower(x)
-    x = x.translate(str.maketrans('', '', string.punctuation))
-    return x
-
-
-def create_soup(x):
-    return ''.join(x['keywords']) + ' ' + ''.join(x['top_cast']) + ' ' + ''.join(x['genres'])
 
 
 def get_recommendations(data,title, cosine_sim,indices):
@@ -31,13 +14,7 @@ def get_recommendations(data,title, cosine_sim,indices):
     return data['id'].iloc[movie_indices]
 
 def run_model(movie_title):
-    data = pd.read_csv('tmdb_movie_info.csv')
-    features = ['keywords', 'genres', 'top_cast']
-    data['overview'] = data['overview'].astype(str)
-    for feature in features:
-        data[feature] = data[feature].apply(clean_data)
-
-    data['soup'] = data.apply(create_soup, axis=1)
+    data = pd.read_csv('movie_model.csv', encoding='utf-8')
 
     count = CountVectorizer(stop_words='english')
     count_matrix = count.fit_transform(data['soup'])
